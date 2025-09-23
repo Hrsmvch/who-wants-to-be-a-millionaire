62afc3533f65916827d29b47953288060d7be2be
Halyna Harasymovych <halyna.harasymovych@tallium.com>
2025-09-23 12:39:22 +0300

    fix: issue with toggle prize panel on safari. feat: add favicon

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
